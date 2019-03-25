ec08ab86a986de66c8c4d9a56ea7a980c1b0755f
Karan Singhal <karan1149@gmail.com>
2019-03-24 23:00:45 -0400

    Guess number simulation

---
+++ guess_number.py
@@ -0,0 +1,28 @@
+MAX_NUM = 1000
+
+def guess_number(x):
+	guesses = 0
+	low = 1
+	high = MAX_NUM
+
+	while low < high:
+
+		guesses += 1
+		mid = (low + high) // 2
+		if mid == x:
+			return guesses
+		if x > mid:
+			low = mid + 1
+		else:
+			high = mid - 1
+	return guesses + 1
+
+guesses = []
+for i in range(1, MAX_NUM + 1):
+	guesses.append(guess_number(i))
+
+print("Average")
+print(sum(guesses) / MAX_NUM)
+
+print("Max")
+print(max(guesses))
